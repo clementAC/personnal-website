d73aee8bb930c3f42f90072bc307bd584f5b4201
Clement <clement@luseed.io>
2020-02-26 18:34:16 +0100

    lang accessibility improvement

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { ThemeContextProvider } from "../context/Theme";
 
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return (
     <ThemeContextProvider isDark={true}>
       <Component {...pageProps} />
     </ThemeContextProvider>
   );
 }
+
+export default MyApp;
